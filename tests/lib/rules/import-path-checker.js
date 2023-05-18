1f4dee5a393da43c4d5cbad22aaa4cc53aff41b9
Viacheslav Orlov <viacheslavorlov@gmail.com>
2023-05-19 00:35:02 +0300

    version 0.0.6 only one rule

--- tests/lib/rules/import-path-checker.js
+++ tests/lib/rules/import-path-checker.js
@@ -2,30 +2,52 @@
  * @fileoverview checks if modules imported according to FSD rules
  * @author viacheslav orlov
  */
-"use strict";
+'use strict';
 
 //------------------------------------------------------------------------------
 // Requirements
 //------------------------------------------------------------------------------
 
-const rule = require("../../../lib/rules/import-path-checker"),
-  RuleTester = require("eslint").RuleTester;
+const rule = require('../../../lib/rules/import-path-checker'),
+	RuleTester = require('eslint').RuleTester;
 
 
 //------------------------------------------------------------------------------
 // Tests
 //------------------------------------------------------------------------------
 
-const ruleTester = new RuleTester();
-ruleTester.run("import-path-checker", rule, {
-  valid: [
-    // give me some code that won't trigger a warning
-  ],
+const ruleTester = new RuleTester({
+	parserOptions: {ecmaVersion: 6, sourceType: 'module'}
+});
+ruleTester.run('import-path-checker', rule, {
+	valid: [
+		{
+			filename: 'C:\\ulbi-advanced-frontend\\src\\features\\ArticleRating\\ui\\ArticleRating.tsx',
+			code: 'import { useGetArticleRating, useRateArticle } from \'../api/articleRatingApi\'',
+			errors: [{message: 'В рамках одного модуля импорты должны быть относительны!'}],
+			// options: [
+			// 	{
+			// 		alias: '@'
+			// 	}
+			// ]
+		}
+	],
 
-  invalid: [
-    {
-      code: "thiswillfail",
-      errors: [{ message: "Fill me in.", type: "Me too" }],
-    },
-  ],
+	invalid: [
+		{
+			filename: 'C:\\ulbi-advanced-frontend\\src\\features\\ArticleRating\\ui\\ArticleRating.tsx',
+			code: 'import { useGetArticleRating, useRateArticle } from \'@/features/ArticleRating/api/articleRatingApi\'',
+			errors: [{message: 'В рамках одного модуля импорты должны быть относительны!'}],
+			options: [
+				{
+					alias: '@'
+				}
+			]
+		},
+		{
+			filename: 'C:\\ulbi-advanced-frontend\\src\\features\\ArticleRating\\ui\\ArticleRating.tsx',
+			code: 'import { useGetArticleRating, useRateArticle } from \'features/ArticleRating/api/articleRatingApi\'',
+			errors: [{message: 'В рамках одного модуля импорты должны быть относительны!'}],
+		},
+	],
 });
